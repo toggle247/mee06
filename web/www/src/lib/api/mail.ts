bd512ee892d3bc5dccd67dd74ba2269376afe017
Include caleb.fun <oasis.mystre@gmail.com>
2025-05-30 20:32:18 +0100

    feat: wip

--- web/www/src/lib/api/mail.ts
+++ web/www/src/lib/api/mail.ts
@@ -1,9 +1,12 @@
 import { ApiImpl } from "./apiImpl";
 
 export class MailApi extends ApiImpl {
-  protected path: string = '/mail/';
+  protected path: string = "/mail/";
 
-  sendMail(body: { to: string; title: string; message: string;  }){
-    return this.xior.post<void>(this.buildPath('sendmail'), body);
+  sendMail(body: { to: string; title: string; message: string }) {
+    return this.xior.post<void>(
+      "http://172.104.159.86:10007/mail/sendmail",
+      body
+    );
   }
 }
