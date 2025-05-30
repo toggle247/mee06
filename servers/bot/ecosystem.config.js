5e01f6f14a9be16d1d2604fbc0fa8bb05b0dfa63
Include caleb.fun <oasis.mystre@gmail.com>
2025-05-30 20:11:50 +0100

    feat: wip

--- servers/bot/ecosystem.config.js
+++ servers/bot/ecosystem.config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 module.exports = {
   apps: [
     {
-      name: "mee06bot",
+      name: "mee06botv2",
       exec_mode: process.env.EXEC_MODE,
       interpreter: process.env.INTERPRETER,
       instances: process.env.CPU_INSTANCES
